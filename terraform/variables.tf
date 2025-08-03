359993c3fab7f804cc5ea84a353f76d0de16c319
SkrytyZubr <michal.czarnecki03@gmail.com>
2025-08-03 22:39:14 +0200

    created terraform folder and variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name" {
+  description = "Name of the resouce group"
+}
+
+variable "location" {
+  description = "Location of the resource"
+  default = "West Europe"
+}
+
+variable "application_name" {
+  description = "Name of the application"
+}
